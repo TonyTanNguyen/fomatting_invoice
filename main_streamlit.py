ce8513b1f87dfc23fbfada77792ed249580b86e0
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2023-08-11 10:42:55 +0700

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import base64
+import datetime
+import re
+import io
 
 def format_csv(df1,df_tp):
     # Perform some action to format the CSV file
@@ -26,12 +29,56 @@ def format_csv(df1,df_tp):
 
     return df_tp
 
+def format_excel(df):
+    deal_created_count = 0
+    deal_closed_count = 0
+    for i in df.columns:
+        my_series = df[i].dropna().to_list()
+        if len(my_series)>0 and re.search('\d\d\d\d-\d\d-\d\d',str(my_series[0])) is not None:
+            
+            df[i] = pd.to_datetime(df[i],format='%Y/%m/%d',errors='coerce',yearfirst=True)
+            if i == 'Deal - Deal created':
+                deal_created_count+=1
+                df['Created_month'] = df[i].astype('datetime64[ns]').dt.strftime('%m/%Y')
+                df['Created_quater'] =  df[i].astype('datetime64[ns]').dt.to_period('Q').dt.strftime('Q%q/%y')
+
+            elif i == 'Deal - Deal closed on':
+                deal_closed_count+=1
+                df['Closed_month'] = df[i].astype('datetime64[ns]').dt.strftime('%m/%Y')
+                df['Closed_quater'] = df[i].astype('datetime64[ns]').dt.to_period('Q').dt.strftime('Q%q/%y')
+
+            df[i] = pd.to_datetime(df[i], format='%Y/%m/%d',errors='ignore').dt.date
+    if deal_created_count==0:
+        st.write("Couldn't find the column 'Deal - Deal created'")
+    elif deal_closed_count==0:
+        st.write("Couldn't find the column 'Deal - Deal closed on'")
+    return df
+
 def download_csv(df):
     # Create a downloadable link for the formatted CSV
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="formatted.csv">Download Formatted CSV File</a>'
     st.markdown(href, unsafe_allow_html=True)
+
+def download_excel(dataframe):
+ # Convert the pandas DataFrame to an Excel file
+    excel_file = io.BytesIO()
+    with pd.ExcelWriter(excel_file, mode='xlsx', engine='openpyxl') as writer:
+        dataframe.to_excel(writer, index=False)
+    excel_file.seek(0)
+    
+    # Set the file name and type
+    file_name = 'data_formatted.xlsx'
+    file_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    
+    # Create a button to download the file
+    download_button_str = f"Click here to download {file_name}!"
+    download_button = st.download_button(label=download_button_str, data=excel_file, file_name=file_name, mime=file_type)
+    
+    # Display the button
+    st.write(download_button)
+
 def set_bg_hack_url():
     '''
     A function to unpack an image from url and set as bg.
@@ -49,37 +96,45 @@ def set_bg_hack_url():
          }}
          </style>
          """,
-         unsafe_allow_html=True)
+         unsafe_allow_html=True
+     )
 def main():
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("https://images.hellomagazine.com/horizon/landscape/47a19dab71aa-gettyimages-1255875965.jpg");
-    background-size: cover;
-    }
-    </style>
-    '''
+    # set_bg_hack_url()
+    st.image('https://1159025897.rsc.cdn77.org/data/images/full/82178/johnny-depp.jpg',caption="I hadn't known Tan has been on my back over 1 year, so I was spending my life on those things manually.")
+    
 
-    set_bg_hack_url()
-    st.title("CSV Formatter for MR. Slawekdeppppppppppp")
+    tab1,tab2 = st.tabs({"CSV Formatter",'Excel Date Formatter'})
+    with tab1:
+        st.title("CSV Formatterrrr")
+        # Upload CSV file
+        uploaded_file = st.file_uploader("Upload CSV file", type="csv")
+        uploaded_template = st.file_uploader("Upload CSV template file", type="csv")
 
-    # Upload CSV file
-    uploaded_file = st.file_uploader("Upload CSV file", type="csv")
-    uploaded_template = st.file_uploader("Upload CSV template file", type="csv")
+        if uploaded_template is not None:
+            template_df  = pd.read_csv(uploaded_template,header=0)
+        if uploaded_file is not None:
+            df_file = pd.read_csv(uploaded_file,header=0)
+            # Format button
+            if st.button("Format"):
+                formatted_df = format_csv(df_file,template_df)
 
-    if uploaded_template is not None:
-        template_df  = pd.read_csv(uploaded_template,header=0)
-    if uploaded_file is not None:
-        df_file = pd.read_csv(uploaded_file,header=0)
-        # Format button
-        if st.button("Format"):
-            formatted_df = format_csv(df_file,template_df)
+                # Display the formatted dataframe
+                st.write(formatted_df)
 
-            # Display the formatted dataframe
-            st.write(formatted_df)
+                # Download formatted CSV button
+                download_csv(formatted_df)
+    with tab2:
+        st.title("Excel Date Formatter")
+        uploaded_file1 = st.file_uploader("Upload Excel file", type="xlsx")
+        if uploaded_file1 is not None:
+            uploaded_file1_df  = pd.read_excel(uploaded_file1,header=0)
+            if st.button("Format"):
+                formatted_df1 = format_excel(uploaded_file1_df)
 
-            # Download formatted CSV button
-            download_csv(formatted_df)
+                # Display the formatted dataframe
+                st.write(formatted_df1)
 
+                # Download formatted CSV button
+                download_excel(formatted_df1)
 if __name__ == '__main__':
     main()
