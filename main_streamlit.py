c3d7351c59980d5b15714635f32e6fea5eec536d
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2023-09-20 11:30:38 +0700

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -103,7 +103,7 @@ def main():
     st.image('https://1159025897.rsc.cdn77.org/data/images/full/82178/johnny-depp.jpg',caption="I hadn't known Tan has been on my back over 1 year, so I was spending my life on those things manually.")
     
 
-    tab2,tab1 = st.tabs({"CSV Formatter",'Excel Date Formatter'})
+    tab1,tab2 = st.tabs({"CSV Formatter",'Excel Date Formatter'})
     with tab1:
         st.title("CSV Formatterrrr")
         # Upload CSV file
