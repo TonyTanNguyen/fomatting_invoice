f06b71ef5ea273256eb3776b647b865b03ffe4c7
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2023-10-12 10:26:47 +0000

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -15,7 +15,7 @@ def format_csv(df1,df_tp):
     else:
         st.write(f'Missing Customer Name')
     if 'PO Number' in cols:
-        df_tp['InventoryItemCode'] = '.'
+        df_tp['InventoryItemCode'] = df1['PO Number']
     else:
         st.write(f'Missing PO Number')
     if 'Primary Contact EmailID' in cols:
@@ -56,7 +56,8 @@ def format_csv(df1,df_tp):
     else:
         st.write(f'Missing Total')
     if 'Item Desc' in cols:
-        df_tp['*Description'] = df1.apply(lambda x: x['Item Desc'] + ' Purchase Order: ' + str(x['PO Number']) if x['PO Number'] not in ['',np.nan] else x['Item Desc'],axis=1)
+        df_tp['*Description'] = df1['Item Desc']
+        # df_tp['*Description'] = df1.apply(lambda x: x['Item Desc'] + ' Purchase Order: ' + str(x['PO Number']) if x['PO Number'] not in ['',np.nan] else x['Item Desc'],axis=1)
         # df_tp['*Description'] = df1['Item Desc'] + '\nPurchase Order: ' + df1['PO Number']
     else:
         st.write(f'Missing Item Desc')
