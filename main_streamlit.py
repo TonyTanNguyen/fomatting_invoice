a1fbb61d98d9b9ea215511b10d6f8c1b75c537c2
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2023-10-23 17:03:02 +0700

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -345,6 +345,14 @@ def main():
                 options = Options()
                 # options.add_argument('--disable-gpu')
                 options.add_argument('--headless')
+                options.add_argument('--no-sandbox')
+                options.add_argument("window-size=1920,1080")
+                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument('--headless')
+                options.add_argument("disable-infobars")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-dev-shm-usage")
+                
                 service = Service()
                 browser = webdriver.Chrome(service=service, options=options)
                 
