83ac57fbd5fe60a52d85098cf6950d2638734d08
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2023-10-23 16:27:32 +0700

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -339,8 +339,8 @@ def main():
                 alert = st.empty()
                 
                 service = Service()
-                # options = webdriver.ChromeOptions()
-                options = Options()
+                options = webdriver.ChromeOptions()
+                # options = Options()
                 options.add_argument("--headless")
                 options.add_argument("--no-sandbox")
                 options.add_argument("--disable-dev-shm-usage")
