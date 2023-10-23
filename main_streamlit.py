3a2d032fb07bf42dcb6f78d5c6692995eb9c68d0
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2023-10-23 16:24:46 +0700

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -11,6 +11,7 @@ import csv
 
 
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
